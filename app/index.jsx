88950648da3d11b3244a2123cacf7ad40ff3fbac
Aryan Thacker <randomthacker@gmail.com>
2024-08-11 09:34:38 +0530

    feat: app index

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,64 @@
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import { CustomButton } from "../components/CustomButton";
+import { Loader } from "../components/Loader";
+// import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
-    return (
-        <View className="flex-1 items-center justify-center bg-red-500">
-            <Text className="text-2xl">Testing app</Text>
-            <Link href="/profile">Click here</Link>
-            <StatusBar style="auto" />
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      {/* <Loader isLoading={loading} /> */}
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"    aryan is very popular and is getting called by public speaking I am doing a job with eighty thousand salary i
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          {/* <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          /> */}
         </View>
-    );
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
 }
